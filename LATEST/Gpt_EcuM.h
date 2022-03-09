bafb93565f7c9361c1e32e7d97329d4dc6b2ebfc
root <nagaraja.hm.0001@gmail.com>
2022-03-09 12:54:08 +0100

    Update: submodule Gpt

--- LATEST/Gpt_EcuM.h
+++ LATEST/Gpt_EcuM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infEcuMClient *gptrinfEcuMClient_Gpt;
+extern infEcuMClient* gptrinfEcuMClient_Gpt;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
--- LATEST/Gpt_SchM.h
+++ LATEST/Gpt_SchM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infSchMClient *gptrinfSchMClient_Gpt;
+extern infSchMClient* gptrinfSchMClient_Gpt;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
