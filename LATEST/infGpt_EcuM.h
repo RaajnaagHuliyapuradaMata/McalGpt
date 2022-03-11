84c9ed5ad3a3a05bbd01facaa1e17152f982a5b0
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:12:28 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Gpt

--- LATEST/infGpt_EcuM.h
+++ LATEST/infGpt_EcuM.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Gpt.cpp                                  */
+/* File   : infGpt_EcuM.h                            */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infGpt_EcuM.h"
-#include "infGpt_Dcm.h"
-#include "infGpt_SchM.h"
+#include "Compiler_Cfg_Gpt.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Gpt:
-      public abstract_module
-{
-   public:
-      FUNC(void, GPT_CODE) InitFunction   (void);
-      FUNC(void, GPT_CODE) DeInitFunction (void);
-      FUNC(void, GPT_CODE) GetVersionInfo (void);
-      FUNC(void, GPT_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,60 +32,11 @@ class module_Gpt:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Gpt     Gpt;
-infEcuMClient* gptrinfEcuMClient_Gpt = &Gpt;
-infDcmClient*  gptrinfDcmClient_Gpt  = &Gpt;
-infSchMClient* gptrinfSchMClient_Gpt = &Gpt;
+extern infEcuMClient* gptrinfEcuMClient_Gpt;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, GPT_CODE) module_Gpt::InitFunction(void){
-}
-
-FUNC(void, GPT_CODE) module_Gpt::DeInitFunction(void){
-}
-
-FUNC(void, GPT_CODE) module_Gpt::GetVersionInfo(void){
-}
-
-FUNC(void, GPT_CODE) module_Gpt::MainFunction(void){
-}
-
-#include "Gpt_Unused.h"
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::GetTimeElapsed(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::GetTimeRemaining(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::StartTimer(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::StopTimer(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::DisableNotification(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::EnableNotification(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::SetMode(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::CheckWakeup(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::DisableWakeup(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::EnableWakeup(void){
-}
-
-FUNC(void, GPT_CODE) class_Gpt_Unused::GetPredefTimerValue(void){
-}
 
 /*****************************************************/
 /* EOF                                               */
