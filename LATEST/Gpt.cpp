fe535b5db8aed7e7ee9a94b8e5f643d0a32ada98
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:46 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Gpt

--- LATEST/Gpt.cpp
+++ LATEST/Gpt.cpp
@@ -40,6 +40,16 @@ class module_Gpt:
       FUNC(void, GPT_CODE) DeInitFunction (void);
       FUNC(void, GPT_CODE) GetVersionInfo (void);
       FUNC(void, GPT_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, GPT_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
