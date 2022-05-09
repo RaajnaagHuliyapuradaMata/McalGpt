cef2f168b3d06a42e7bd3af675d2fe4e3b837723
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:30 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Gpt

--- LATEST/Gpt.cpp
+++ LATEST/Gpt.cpp
@@ -35,6 +35,13 @@ class module_Gpt:
       public abstract_module
    ,  public class_Gpt_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Gpt(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
