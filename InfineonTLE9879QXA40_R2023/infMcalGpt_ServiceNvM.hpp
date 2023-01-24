424b95ac8f0e6be2fdb5c2fb2c732308d8ca2548
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:36 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for general purpose timers

---
+++ InfineonTLE9879QXA40_R2023/infMcalGpt_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalGpt_ServiceNvM.hpp                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALGPT_AR_RELEASE_VERSION_MAJOR                                  4
+#define CONSTMCALGPT_AR_RELEASE_VERSION_MINOR                                  3
+
+#define Const_DefaultInit_ConstMcalGpt                                                                           \
+   ,  {CONSTMCALGPT_AR_RELEASE_VERSION_MAJOR, CONSTMCALGPT_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
