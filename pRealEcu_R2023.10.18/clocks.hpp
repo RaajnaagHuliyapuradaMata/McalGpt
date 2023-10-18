30d7910a55e545a3c412d1cc1c98fb7edac8e237
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-18 14:21:34 +0200

    AUTOSAR - code freeze - pRealEcu_cR_R2023.10.18 - McalGpt

---
+++ pRealEcu_R2023.10.18/clocks.hpp
@@ -0,0 +1,28 @@
+
+
+#ifndef _CLOCKS_H
+#define _CLOCKS_H
+
+#include "clocksX.hpp"
+
+//void InternTimersInit(void);
+//void IncrInternMsTimer(void);
+//void GetInternMSTime(uint32* ulInternalTime);
+//void UpdateStanbyTimer(boolean bIgnState, uint16 ushVSpeed, uint8 ucVmin);
+//void ResetStanbyTimer(void);
+//boolean bStanbyTimerElapsed(void);
+//void SkipStanbyTimer(void);
+//uint8 ucGetCurrentECUParkingState(void);
+//uint16 ushGetCurrentECUStanbyTime(void);
+//void UpdateParkingTimer(uint16 ushVSpeed);
+//uint16 ushGetCurrentECUParkingTime(void);
+//uint16 ushGetCurrentECUKl15OffParkingTime(void);
+//uint8 ucGetCurrentECUAfdStartState(void);
+//void GetSystemOperatingTime(uint32* ulOpTime);
+//uint8 GetOpTimeMehodTarget(void);
+//uint8 GetPalAbsConstantDelay(void);
+//uint8  GetOpTimeMethodCurrent(void);
+//void SetOpTimeMethodTarget(uint8 ucTimeMethod, uint8 ucCstDelay);
+//void SetPalAbsTimeInfoSNA(void);
+
+#endif
