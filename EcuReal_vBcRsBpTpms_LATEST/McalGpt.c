f84c6be0d8de636555591d9edab95d34b5dadb8d
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2023-11-20 08:19:50 +0100

    AUTOSAR Migration Activities - EcuReal_vBcRsBpTpms_LATEST - McalGpt

---
+++ EcuReal_vBcRsBpTpms_LATEST/McalGpt.c
@@ -0,0 +1,116 @@
+/******************************************************************************/
+/* File   : McalGpt.c                                                         */
+/*                                                                            */
+/* Author : Raajnaag HULIYAPURADA MATA                                        */
+/*                                                                            */
+/* License / Warranty / Terms and Conditions                                  */
+/*                                                                            */
+/* Everyone is permitted to copy and distribute verbatim copies of this lice- */
+/* nse document, but changing it is not allowed. This is a free, copyright l- */
+/* icense for software and other kinds of works. By contrast, this license is */
+/* intended to guarantee your freedom to share and change all versions of a   */
+/* program, to make sure it remains free software for all its users. You have */
+/* certain responsibilities, if you distribute copies of the software, or if  */
+/* you modify it: responsibilities to respect the freedom of others.          */
+/*                                                                            */
+/* All rights reserved. Copyright © 1982 Raajnaag HULIYAPURADA MATA           */
+/*                                                                            */
+/* Always refer latest software version from:                                 */
+/* https://github.com/RaajnaagHuliyapuradaMata?tab=repositories               */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Std_Types.hpp"
+
+#include "infMcalGptSwcApplEcuM.hpp"
+
+#include "uC_Gpt.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+#ifndef ReSim
+#else
+volatile       uint32 PROTCMD0;
+volatile const uint32 PROTS0;
+volatile       uint32 MOSCE;
+volatile const uint32 MOSCS;
+volatile       uint32 MOSCC;
+volatile       uint32 MOSCST;
+volatile       uint32 PROTCMD1;
+volatile const uint32 PROTS1;
+volatile       uint32 PLLE;
+volatile const uint32 PLLS;
+volatile       uint32 PLLC;
+volatile       uint32 CKSC_CPUCLKS_CTL;
+volatile const uint32 CKSC_CPUCLKS_ACT;
+volatile       uint32 CKSC_CPUCLKD_CTL;
+volatile const uint32 CKSC_CPUCLKD_ACT;
+#endif
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, MCALGPT_CODE) infMcalGptSwcApplEcuM_InitFunction(void){
+   MOSCC  = 0x02; //TBD: Move to CfgMcalGpt
+   MOSCST = 0xFFFF;
+   protected_write(
+         PROTCMD0
+      ,  PROTS0
+      ,  MOSCE
+      ,  0x01
+   );
+   while((MOSCS & 0x04) != 0x4);
+
+   PLLC = 0x00000a27;
+   protected_write(
+         PROTCMD1
+      ,  PROTS1
+      ,  PLLE
+      ,  0x01
+   );
+   while((PLLS & 0x04) != 0x04);
+
+   protected_write(
+         PROTCMD1
+      ,  PROTS1
+      ,  CKSC_CPUCLKD_CTL
+      ,  0x01
+   );
+   while(CKSC_CPUCLKD_ACT != 0x01);
+
+   protected_write(
+         PROTCMD1
+      ,  PROTS1
+      ,  CKSC_CPUCLKS_CTL
+      ,  0x03
+   );
+   while(CKSC_CPUCLKS_ACT != 0x03);
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
